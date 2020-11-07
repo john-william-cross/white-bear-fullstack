e005d39207e1b5b024407e44f361be2828c04fcb
John Cross <john.william.cross@gmail.com>
2020-11-06 16:33:06 -0800

    end of 339A creating a server and accessing our database

--- server.js
+++ server.js
@@ -10,12 +10,35 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", (error, results, fields) => {
-   if (error) {
-      console.log(error);
-   } else {
-      console.log("The solution is: ", results[0].solution);
+//select all these columns
+// from this table of users
+// then inner join xref_user_hobbies with users ON user_id(from xref_user_hobbies), users.id matches that
+// then inner join the table of hobbies on hobbies.id (id inside of hobbies), join that with table of xref_user_hobbies, which has a matching hobby_id inside of it
+//where users first name = 'mike'
+connection.query(
+   `
+    SELECT  
+        users.id AS user_id,
+        users.first_name,
+        users.last_name,
+        hobbies.id AS hobby_id,
+        hobbies.name AS hobby_name
+    FROM 
+        users
+    INNER JOIN 
+        xref_user_hobbies ON user_id = users.id
+    INNER JOIN 
+        hobbies ON hobbies.id = xref_user_hobbies.hobby_id 
+    WHERE 
+        users.first_name = 'mike'
+    `,
+   (err, res) => {
+      if (err) {
+         console.log(err);
+      } else {
+         console.log(res);
+      }
    }
-});
+);
 
 connection.end();
