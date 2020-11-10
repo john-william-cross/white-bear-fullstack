0e7b27aff663080c5b08783dfe63efcbe079e6a1
John Cross <john.william.cross@gmail.com>
2020-11-10 10:49:18 -0800

    end of 345A increasing security with prepared statement syntax

--- api/v1/memory-cards.js
+++ api/v1/memory-cards.js
@@ -8,9 +8,22 @@ const selectAllCards = require("../../queries/selectAllCards");
 //@desc         Get all memory cards for a user by search term and order
 //@access       PUBLIC
 router.get("/", (req, res) => {
+   // get request, post  requests.... this is where all the business logic happens in an application.
    console.log(req.query);
    const { userId, searchTerm, order } = req.query;
-   db.query(selectAllCards(userId, searchTerm, order))
+   let constructedSearchTerm;
+   if (searchTerm === "" || searchTerm === undefined) {
+      constructedSearchTerm = "%%";
+   } else {
+      constructedSearchTerm = `%${searchTerm}%`;
+   }
+   /* https://www.npmjs.com/package/mysql#escaping-query-values */
+   db.query(selectAllCards, [
+      userId,
+      constructedSearchTerm,
+      constructedSearchTerm,
+      order,
+   ])
       .then((dbRes) => {
          // console.log(dbRes);
          res.json(dbRes);
