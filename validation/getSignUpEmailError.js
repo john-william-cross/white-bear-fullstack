183ca8e138b8738c1f6d10ad0ad3a830cfe8ed0b
John Cross <john.william.cross@gmail.com>
2020-11-17 08:50:17 -0800

    41:50 into 356A but receiving errors: ECONNREFUSED

--- validation/getSignUpEmailError.js
+++ validation/getSignUpEmailError.js
@@ -1,3 +1,25 @@
+const db = require("../db");
+const { EMAIL_REGEX } = require("../utils/helpers");
+const selectUserByEmail = require("../queries/selectUserByEmail ");
+
 module.exports = function getSignUpEmailError(email) {
+   if (email === "") {
+      return "Please enter your email address";
+   }
+   if (EMAIL_REGEX.test(email.toLowerCase()) === false) {
+      return "Please enter a valid email address";
+   }
+   if (checkIsInDb(email)) {
+      return "This email address already exists in the database.";
+   }
    return "";
 };
+
+function checkIsInDb(email) {
+   return db
+      .query(selectUserByEmail, email)
+      .then((users) => {
+         console.log(users);
+      })
+      .catch((err) => {});
+}
