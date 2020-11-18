b4019dd934a9f8caf2b5d58844b17d818faf5537
John Cross <john.william.cross@gmail.com>
2020-11-17 18:19:05 -0800

    save at 21 minutes into 357a

--- validation/getSignUpPasswordError.js
+++ validation/getSignUpPasswordError.js
@@ -1,3 +1,23 @@
 module.exports = function getSignUpPasswordError(password, email) {
-   return "";
+   const uniqChars = [...new Set(password)];
+
+   if (password === "") {
+      return "Please create a password.";
+   }
+   if (password.length < 9) {
+      return "Your password must be at least 9 characters.";
+   }
+   if (this.checkHasLocalPart(password, email)) {
+      return "For your safety, your password cannot contain your email address.";
+   }
+   if (uniqChars.length < 3) {
+      return "For your safety, your password must contain at least 3 unique characters.";
+   }
 };
+
+function checkHasLocalPart(password, email) {
+   const localPart = email.split("@")[0];
+   if (localPart === "") return false;
+   else if (localPart.length < 4) return false;
+   else return password.includes(localPart);
+}
