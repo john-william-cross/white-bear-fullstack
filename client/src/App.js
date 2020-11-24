653f20ecacd46de99095ff7b3a6f4dbdf2c488bc
John Cross <john.william.cross@gmail.com>
2020-11-24 10:58:05 -0800

    end 359A Sending and Receiving a JSON Web Token

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,36 @@ import ReviewEmpty from "./component/pages/ReviewEmpty";
 import AllCards from "./component/pages/AllCards";
 import Edit from "./component/pages/Edit";
 import NotFound from "./component/pages/NotFound";
+import jwtDecode from "jwt-decode";
+import store from "./store/store";
+import actions from "./store/actions";
 
 const authToken = localStorage.authToken;
 if (authToken) {
    // if the authToken is not expired
    const currentTimeInSec = Date.now() / 1000;
-   if (currentTimeInSec > exp) {
+   const user = jwtDecode(authToken);
+   if (currentTimeInSec > user.exp) {
+      console.log("expired token");
+      // remove the currentUser from the global state / redux store
+      store.dispatch({
+         type: actions.UPDATE_CURRENT_USER,
+         payload: {},
+      });
+   } else {
+      console.log("valid token");
+      // store the user in global state / redux store (currentUser)
+      store.dispatch({
+         type: actions.UPDATE_CURRENT_USER,
+         payload: user,
+      });
+      // set authorization headers
+      // redirect to create-answers
+      const currentUrl = window.location.pathname;
+      if (currentUrl === "/") {
+         window.location.href = "/create-answer";
+      }
    }
-   // store the user in global state / redux store (currentUser)
-   // set authorization headers
-   // redirect to create-answers
 } else {
    console.log("no authToken");
 }
--- client/src/store/reducers/currentUser.js
+++ client/src/store/reducers/currentUser.js
@@ -1,14 +1,15 @@
 import actions from "../actions";
+import isEmpty from "lodash/isEmpty";
 
 export default function currentUser(currentUser = {}, action) {
    //every action has an action.type and action.payload
-
    switch (action.type) {
       case actions.UPDATE_CURRENT_USER:
+         if (isEmpty(action.payload)) {
+            localStorage.removeItem("authToken");
+         }
          return action.payload;
       default:
          return currentUser;
    }
 }
-
-///awefawefasefasdfasdfwe
